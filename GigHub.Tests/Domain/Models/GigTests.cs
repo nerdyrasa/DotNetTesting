bff5563dfffa0bf6b37860ec67df6d889522ee28
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-09-19 11:33:59 -0500

    Add tests for Gig class.

---
+++ GigHub.Tests/Domain/Models/GigTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using GigHub.Core.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace GigHub.Tests.Domain.Models
+{
+    [TestClass]
+    public class GigTests
+    {
+
+        [TestMethod]
+        public void Cancel_ExistingGigCanceled_ReturnsIsCanceledTrue()
+        {
+            var gig = new Gig();
+
+            gig.Cancel();
+
+            gig.IsCanceled.Should().BeTrue();
+
+        }
+
+        [TestMethod]
+        public void Cancel_WhenCalled_EachAttendeeGetsANotification()
+        {
+            var gig = new Gig();
+            gig.Attendances.Add(new Attendance { Attendee = new ApplicationUser { Id = "1" } });
+
+            gig.Cancel();
+
+            // TODO: Add gig.GetAttendees() to Gig class
+            var attendees = gig.Attendances.Select(a => a.Attendee).ToList();
+            attendees[0].UserNotifications.Count().Should().Be(1);
+
+        }
+    }
+}
