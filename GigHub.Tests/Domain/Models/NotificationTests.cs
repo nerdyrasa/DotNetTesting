19df0f211d7472f304b9f8fa5d3e8b999b03aae9
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-09-20 10:48:01 -0500

    Added tests for notification class.

---
+++ GigHub.Tests/Domain/Models/NotificationTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GigHub.Core.Models;
+using FluentAssertions;
+
+namespace GigHub.Tests.Domain.Models
+{
+    [TestClass]
+    public class NotificationTests
+    {
+        [TestMethod]
+        public void GigCanceled_WhenCalled_ShouldReturnNotificationForCanceledGig()
+        {
+            var gig = new Gig();
+
+            var notification = Notification.GigCanceled(gig);
+
+            notification.Type.Should().Be(NotificationType.GigCanceled);
+
+            notification.Gig.Should().Be(gig);
+        }
+
+        [TestMethod]
+        public void GigCreated_WhenCalled_ShouldReturnNotificationForCreatedGig()
+        {
+            var gig = new Gig();
+
+            var notification = Notification.GigCreated(gig);
+
+            notification.Type.Should().Be(NotificationType.GigCreated);
+
+            notification.Gig.Should().Be(gig);
+        }
+
+        [TestMethod]
+        public void GigUpdated_WhenCalled_ShouldReturnNotificationForUpdatedGig()
+        {
+            var gig = new Gig();
+
+            var notification = Notification.GigUpdated(gig, DateTime.Now, "Test Venue");
+
+            notification.Type.Should().Be(NotificationType.GigUpdated);
+
+            notification.Gig.Should().Be(gig);
+        }
+    }
+}
